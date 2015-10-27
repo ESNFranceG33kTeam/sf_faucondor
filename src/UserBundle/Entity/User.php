3ade3562fb13a46cd8808615ff0a3414bcd707f9
Jeremie Samson <jeremie.samson76@gmail.com>
2015-10-27 22:31:36 +0100

    Adding FOSUserBundle and DoctrineMigration

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Created by PhpStorm.
+ * User: jerem
+ * Date: 27/10/15
+ * Time: 22:22
+ */
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fo_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
